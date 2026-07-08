39d90b1b90e971eaa4edcf88a4b8dbfcc05ea61f
Claudia Voogt <claudiazondag@gmail.com>
2026-07-08 21:01:00 +0200

    tabblad titel naar ETF Analyzer

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Claudia Voogt",
+  title: "ETF Analyzer",
   description: "Claudia Voogt",
   robots: {
     index: false,
